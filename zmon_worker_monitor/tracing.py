ba976dab4e048cd761337989db940e30b07cc894
Felix Mueller <felix.mueller@zalando.de>
2017-09-14 14:10:38 +0200

    fixed parameter

--- zmon_worker_monitor/tracing.py
+++ zmon_worker_monitor/tracing.py
@@ -1,12 +1,12 @@
 import opentracing
+import logging
 
-
-def init_opentracing_tracer(tracer, debug_level):
+def init_opentracing_tracer(tracer, log_level=logging.WARN):
     if tracer == 'instana':
         import instana.options as instanaOpts  # noqa
         import instana.tracer  # noqa
 
-        instana.tracer.init(instanaOpts.Options(service='zmon-agent', log_level=debug_level))
+        instana.tracer.init(instanaOpts.Options(service='zmon-worker', log_level=log_level))
     elif tracer == 'basic':
         from basictracer import BasicTracer  # noqa
 
